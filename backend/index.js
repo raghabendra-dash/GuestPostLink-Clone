7eb9653e277487119171ca9bf611867d6f547f44
Raghabendra Dash <raghabendradash779@gmail.com>
2025-04-07 01:47:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://backlinkvista.vercel.app"
+  "https://guest-post-link-clone-front.vercel.app/"
 ];
 
 // Middleware
